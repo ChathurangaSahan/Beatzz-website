b021c6a0d1edd2cd16ce520c00cb519a708e7c95
Haputhanthrige Sahan Chathuranga Samarasinghe <128349286+ChathurangaSahan@users.noreply.github.com>
2024-04-01 10:11:37 +0530

    Add files via upload

---
+++ w1953208/page_links.js
@@ -0,0 +1,35 @@
+/*   Name    : Haputhanthrige Sahan Chathuranga Samarasinghe
+     IIT No. : 20220334
+     UoW No. : w1953208 */
+  
+// java script used by navigation bar to redirect to specific pages when clicked on respective buttons
+document.getElementById("home").addEventListener("click", function(){
+    document.location.href="main_page.html"
+})
+document.getElementById("topSongs").addEventListener("click", function(){
+    document.location.href="top_songs.html"
+})
+document.getElementById("contactUs").addEventListener("click", function(){
+    document.location.href="contact_us.html"
+})
+document.getElementById("gallery").addEventListener("click", function(){
+    document.location.href="gallery.html"
+})
+document.getElementById("quiz").addEventListener("click", function(){
+    document.location.href="QuizHome.html"
+})
+document.getElementById("aboutUs").addEventListener("click", function(){
+    document.location.href="AboutUs.html"
+})
+document.getElementById("shop").addEventListener("click", function(){
+    document.location.href="buyProducts.html"
+})
+document.getElementById("popCulture").addEventListener("click", function(){
+    document.location.href="popCulture.html"
+})
+document.getElementById("bestConcerts").addEventListener("click", function(){
+    document.location.href="concerts.html"
+})
+document.getElementById("awards").addEventListener("click", function(){
+    document.location.href="MusicAwards.html"
+})
